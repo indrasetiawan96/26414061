6282348bb711f87577c0b32c855cb06b4139b959
indrasetiawan96 <m26414061@john.petra.ac.id>
2016-09-23 16:54:23 +0700

    Tugas Mngguan

---
+++ pola.sh
@@ -0,0 +1,16 @@
+echo "Masukkan Jumlah n: ";
+
+read n;
+
+
+for((i=0;i<n;i++))
+{
+        for((j=0;j<=i;j++))
+        {
+                echo -n "*";
+        }
+        echo -e;
+}
+git add pola.sh
+git commit -m "Tugas Mngguan"
+git push
