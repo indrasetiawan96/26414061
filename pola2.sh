258ff4ae6b41236244436a3928385bf4101ef419
indrasetiawan96 <m26414061@john.petra.ac.id>
2016-09-23 17:09:55 +0700

    Tugas Commit 23/09

---
+++ pola2.sh
@@ -0,0 +1,32 @@
+o"Masukkan Jumlah n";
+read n;
+
+for((i=$n;i>=1;i--))
+        {
+                for((j=i;j<=$n;j++))
+                {
+                        echo -n " ";
+                }
+                for((k=i;k>=1;k--))
+                {
+                        echo -n " *";
+                }
+                echo -e;
+        }
+
+
+for((i=2;i<=$n;i++))
+        {
+                for((j=$n;j>=i;j--))
+                {
+                        echo -n " ";
+                }
+                for((k=1;k<=i;k++))
+                {
+                        echo -n " *";
+                }
+                echo -e;
+        }
+git add pola2.sh
+git commit -m "Tugas Commit 23/09"
+git push
