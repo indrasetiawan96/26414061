404ecf5d11c0da219a1f093a9180db050800c103
indrasetiawan96 <m26414061@john.petra.ac.id>
2016-09-25 14:59:49 +0700

    Tugas 25/09

---
+++ pola6.sh
@@ -0,0 +1,30 @@
+o "MENU HARI INI";
+echo "-------------";
+echo "1. Bakso     ";
+echo "2. Gado-Gado ";
+echo "3. Exit      ";
+read -p "Pilihan anda [1-3] :" pil;
+
+if [ $pil -eq 1 ]; 
+then
+   echo "Banyak mangkok= ";
+   read jum
+   let bayar=jum*1500;
+elif [ $pil -eq 2 ]; 
+then
+   echo "Banyak porsi= ";
+   read jum
+   let bayar=jum*2000;
+elif [ $pil -eq 3 ]; 
+then
+   exit 0
+else
+   echo "Sorry, tidak tersedia"
+   exit 1
+fi
+
+echo "Harga bayar = Rp. $bayar"
+echo "Terimakasih mz mb"
+git add pola6.sh
+git commit -m "Tugas 25/09"
+git push
