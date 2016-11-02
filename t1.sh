a4cf0a23c3f56a13e617ca6c853dbcad2f9c1fe4
indrasetiawan96 <m26414061@john.petra.ac.id>
2016-11-02 18:09:45 +0700

    Tugas Commit 2/11

---
+++ t1.sh
@@ -0,0 +1,10 @@
+echo -n "Nama anda :"
+read nama;
+
+echo    "Hi $nama,  apa kaabarmu";
+echo    "Pesan dan kesan :";
+read 
+echo    "kata $nama, $REPLY";
+git add t1.sh
+git commit -m "Tugas Commit 2/11"
+git push
